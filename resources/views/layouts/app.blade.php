863efc594f051c272bb23f5b825b591d04b0d32e
mohamedellithy <develomohamed22@gmail.com>
2023-03-30 17:28:39 +0200

    dddjdddddd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,14 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'تجارة شوب') }}</title>
+    <link rel="icon" href="{{ asset('assets/landing/images/favicon.ico') }}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="تجارة شوب هو موقع للعمل كتاجر اونلاين عن طريق التسويق لى منتاجات الموقع و تحقيق الارباح من خلال بيعها">
+    <meta name="keywords" content="تجارة شوب,تجارة,tigara">
+    <meta name="author" content="تجارة شوب">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
